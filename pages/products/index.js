d8a06854ac086824d5647121c96f511b6d9cd08c
saitogen <saitogen921@gmail.com>
2023-02-24 13:31:17 +0900

    商品一覧作成

---
+++ pages/products/index.js
@@ -0,0 +1,11 @@
+import styles from '@/styles/Home.module.css'
+
+export default function ProductsList(){
+    return (
+        <div className={styles.container}>
+            <main className={styles.main}>
+                <h2 className={styles.title}>商品一覧</h2>
+            </main>    
+        </div>
+    );
+}
